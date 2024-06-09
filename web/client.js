bdf13e972e75d1a6d9871e3ffba99cd6378d3086
kimjongappreciator <nonexistentexistentialist1@gmail.com>
2024-06-09 14:53:06 -0500

    Update client.js

--- web/client.js
+++ web/client.js
@@ -6,23 +6,29 @@ const socket = io("ws://localhost:5000"); // Use io instead of new WebSocket
 
 var frames = [];
 
+var startTime;
+var endTime;
+
 socket.on("connect", function() {
   console.log("Conectado al servidor");
 });
 
 socket.on("prediction", function(data) {
   const prediction = data.prediction;
+  endTime = performance.now();
   console.log("Predicción:", prediction);
+  console.log("Tiempo de ejecución:", (endTime - startTime)/1000);
   // Update UI with prediction
 });
 
 navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
   video.srcObject = stream;
+  startTime = performance.now();
   setInterval(function() {
     ctx.drawImage(video, 0, 0);
     const imageData = canvas.toDataURL("image/jpeg");
     //frames.push(imageData);
-
+    
     socket.emit("message", {"frames": imageData}); // Use emit instead of send
 
     /*if (frames.length === 30) {
